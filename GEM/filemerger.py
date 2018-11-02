092a14e7a2be977b321c9dfb3285e0012af4ac13
memesss <massimo.minuti@pi.infn.it>
2018-11-02 15:47:22 +0100

    Add files via upload

---
+++ GEM/filemerger.py
@@ -0,0 +1,31 @@
+def load_data_and_format(file):
+    str = ""
+    f = open(file,'r')
+    line  = f.readline()
+    while line:
+        str = str + '[' + line[:-1] +'],' +'\n'
+        line  = f.readline()
+    f.close
+    return str
+
+if (__name__ == "__main__") :
+    fdest = open("circles_merge.txt",'w')
+
+    fdest.write('[')
+
+    fdest.write(load_data_and_format("Circles_0_0.txt"))
+
+    fdest.write(load_data_and_format("Circles_1701_0.txt"))
+
+    fdest.write(load_data_and_format("Circles_3376_11.txt"))
+
+    fdest.write(load_data_and_format("Circles_5077_64.txt"))
+
+    fdest.write(load_data_and_format("Circles_6779_121.txt"))
+
+    fdest.write(load_data_and_format("Circles_8480_167.txt")[:-2])
+
+    fdest.write(']')
+
+    fdest.close()
+
