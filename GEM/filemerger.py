e1f8f713aaad261d9961cafce61fc3e42de10ea5
memesss <Administrator@pclabgas.pi.infn.it>
2018-11-02 16:43:38 +0100

    started adding files listing

--- GEM/filemerger.py
+++ GEM/filemerger.py
@@ -1,31 +1,40 @@
+from pathlib import Path
+ 
 def load_data_and_format(file):
-    str = ""
-    f = open(file,'r')
-    line  = f.readline()
-    while line:
-        str = str + '[' + line[:-1] +'],' +'\n'
-        line  = f.readline()
-    f.close
-    return str
+	str = ""
+	f = open(file, 'r')
+	line = f.readline()
+	while line:
+		str = str + '[' + line[:-1] + '],' + '\n'
+		line = f.readline()
+	f.close
+	return str
+	
+def list_files (path,extension):
+	p = Path(path)
+	list( p.glob('**/*.txt'))
 
-if (__name__ == "__main__") :
-    fdest = open("circles_merge.txt",'w')
+if (__name__ == "__main__"):
 
-    fdest.write('[')
+	list_files('.','txt')
 
-    fdest.write(load_data_and_format("Circles_0_0.txt"))
+	fdest = open("circles_merge.txt", 'w')
 
-    fdest.write(load_data_and_format("Circles_1701_0.txt"))
+	fdest.write('[')
 
-    fdest.write(load_data_and_format("Circles_3376_11.txt"))
+	fdest.write(load_data_and_format("Circles_0_0.txt"))
 
-    fdest.write(load_data_and_format("Circles_5077_64.txt"))
+	fdest.write(load_data_and_format("Circles_1701_0.txt"))
 
-    fdest.write(load_data_and_format("Circles_6779_121.txt"))
+	fdest.write(load_data_and_format("Circles_3376_11.txt"))
 
-    fdest.write(load_data_and_format("Circles_8480_167.txt")[:-2])
+	fdest.write(load_data_and_format("Circles_5077_64.txt"))
 
-    fdest.write(']')
+	fdest.write(load_data_and_format("Circles_6779_121.txt"))
 
-    fdest.close()
+	fdest.write(load_data_and_format("Circles_8480_167.txt")[:-2])
+
+	fdest.write(']')
+
+	fdest.close()
 
--- GEM/sorter.py
+++ GEM/sorter.py
@@ -9,85 +9,85 @@ class circle():
 		self.corearea	= plist[3]
 #####################################################
 def build_circle_list():
-    file_in = open("circles_merge.txt", 'r')
-    datain = file_in.read()
-    circles = json.loads(datain)
-    file_in.close()
-    circle_list = []
-    for entry in circles:
-        circle_list.append(circle (entry))
-    return circle_list
+	file_in = open("circles_merge.txt", 'r')
+	datain = file_in.read()
+	circles = json.loads(datain)
+	file_in.close()
+	circle_list = []
+	for entry in circles:
+		circle_list.append(circle (entry))
+	return circle_list
 
 if (__name__ == "__main__") :
-    circle_list = build_circle_list()
+	circle_list = build_circle_list()
 
-    print ("found " + str(len(circle_list)) + "circles")
+	print ("found " + str(len(circle_list)) + "circles")
 
-    circle_group0=[]
-    circle_group1=[]
-    circle_group2=[]
-    circle_group3=[]
-    circle_group4=[]
-    circle_group5=[]
+	circle_group0=[]
+	circle_group1=[]
+	circle_group2=[]
+	circle_group3=[]
+	circle_group4=[]
+	circle_group5=[]
 
-    for circle in circle_list:
-        if ( circle.X > 0 and circle.X < 1701):
-            circle_group0.append(circle.radius)
-        if ( circle.X > 1701 and circle.X < 3376):
-            circle_group1.append(circle.radius)
-        if ( circle.X > 3376 and circle.X < 5077):
-            circle_group2.append(circle.radius)
-        if ( circle.X > 5077 and circle.X < 6779):
-            circle_group3.append(circle.radius)
-        if ( circle.X > 6779 and circle.X < 8480):
-            circle_group4.append(circle.radius)
-        if ( circle.X > 8480):
-            circle_group5.append(circle.radius)
+	for circle in circle_list:
+		if ( circle.X > 0 and circle.X < 1701):
+			circle_group0.append(circle.radius)
+		if ( circle.X > 1701 and circle.X < 3376):
+			circle_group1.append(circle.radius)
+		if ( circle.X > 3376 and circle.X < 5077):
+			circle_group2.append(circle.radius)
+		if ( circle.X > 5077 and circle.X < 6779):
+			circle_group3.append(circle.radius)
+		if ( circle.X > 6779 and circle.X < 8480):
+			circle_group4.append(circle.radius)
+		if ( circle.X > 8480):
+			circle_group5.append(circle.radius)
 
-    group0_radius_avg = 0
+	group0_radius_avg = 0
 
-    for radius in circle_group0:
-        group0_radius_avg = group0_radius_avg + radius
+	for radius in circle_group0:
+		group0_radius_avg = group0_radius_avg + radius
 
-    group0_radius_avg = group0_radius_avg / len(circle_group0)
-    print ("group0 radius avg = " + str(group0_radius_avg) )			
+	group0_radius_avg = group0_radius_avg / len(circle_group0)
+	print ("group0 radius avg = " + str(group0_radius_avg) )
 
-    group1_radius_avg = 0
+	group1_radius_avg = 0
 
-    for radius in circle_group1:
-        group1_radius_avg = group1_radius_avg + radius
+	for radius in circle_group1:
+		group1_radius_avg = group1_radius_avg + radius
 
-    group1_radius_avg = group1_radius_avg / len(circle_group1)
-    print ("group1 radius avg = " + str(group1_radius_avg) )
+	group1_radius_avg = group1_radius_avg / len(circle_group1)
+	print ("group1 radius avg = " + str(group1_radius_avg) )
 
-    group2_radius_avg = 0
+	group2_radius_avg = 0
 
-    for radius in circle_group2:
-        group2_radius_avg = group2_radius_avg + radius
+	for radius in circle_group2:
+		group2_radius_avg = group2_radius_avg + radius
 
-    group2_radius_avg = group2_radius_avg / len(circle_group2)
-    print ("group2 radius avg = " + str(group2_radius_avg) )
+	group2_radius_avg = group2_radius_avg / len(circle_group2)
+	print ("group2 radius avg = " + str(group2_radius_avg) )
 
-    group3_radius_avg = 0
+	group3_radius_avg = 0
 
-    for radius in circle_group3:
-        group3_radius_avg = group3_radius_avg + radius
+	for radius in circle_group3:
+		group3_radius_avg = group3_radius_avg + radius
 
-    group3_radius_avg = group3_radius_avg / len(circle_group3)
-    print ("group3 radius avg = " + str(group3_radius_avg) )
+	group3_radius_avg = group3_radius_avg / len(circle_group3)
+	print ("group3 radius avg = " + str(group3_radius_avg) )
 
-    group4_radius_avg = 0
+	group4_radius_avg = 0
 
-    for radius in circle_group4:
-        group4_radius_avg = group4_radius_avg + radius
+	for radius in circle_group4:
+		group4_radius_avg = group4_radius_avg + radius
 
-    group4_radius_avg = group4_radius_avg / len(circle_group4)
-    print ("group4 radius avg = " + str(group4_radius_avg) )
+	group4_radius_avg = group4_radius_avg / len(circle_group4)
+	print ("group4 radius avg = " + str(group4_radius_avg) )
 
-    group5_radius_avg = 0
+	group5_radius_avg = 0
 
-    for radius in circle_group5:
-        group5_radius_avg = group5_radius_avg + radius
+	for radius in circle_group5:
+		group5_radius_avg = group5_radius_avg + radius
 
-    group5_radius_avg = group5_radius_avg / len(circle_group5)
-    print ("group5 radius avg = " + str(group5_radius_avg) )	
+	group5_radius_avg = group5_radius_avg / len(circle_group5)
+	print ("group5 radius avg = " + str(group5_radius_avg) )
